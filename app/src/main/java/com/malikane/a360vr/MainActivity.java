5cafa3a2eb454db611c8ee964acbdb783d748ceb
Muratthekus <murat.kus@ceng.deu.edu.tr>
2019-09-13 13:31:31 +0300

    First commit

---
+++ app/src/main/java/com/malikane/a360vr/MainActivity.java
@@ -0,0 +1,54 @@
+package com.malikane.a360vr;
+
+import android.content.res.AssetManager;
+import android.graphics.BitmapFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.vr.sdk.widgets.pano.VrPanoramaView;
+
+import java.io.InputStream;
+
+public class MainActivity extends AppCompatActivity {
+
+    private VrPanoramaView mVRPanoramaView;
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mVRPanoramaView.pauseRendering();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mVRPanoramaView.resumeRendering();
+    }
+    @Override
+    protected void onDestroy() {
+        mVRPanoramaView.shutdown();
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mVRPanoramaView = (VrPanoramaView) findViewById(R.id.vrPanoramaView);
+        loadPhotoSphere();
+    }
+    private void loadPhotoSphere() {
+        VrPanoramaView.Options options = new VrPanoramaView.Options();
+        InputStream inputStream = null;
+
+        AssetManager assetManager = getAssets();
+        try {
+            inputStream = assetManager.open("image.jpg");
+            options.inputType = VrPanoramaView.Options.TYPE_MONO;
+            mVRPanoramaView.loadImageFromBitmap(BitmapFactory.decodeStream(inputStream), options);
+            inputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
